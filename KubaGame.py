596c38698b125ad9ce35905a970d4cd02029f469
rotorhead7700 <brogdonk@oregonstate.edu>
2021-05-20 20:33:46 -0400

    shelling out class and methods

---
+++ KubaGame.py
@@ -0,0 +1,33 @@
+# Kyle Brogdon
+# 20 MAY 2021
+# Program that creates a KubaGame board, CS 162's final project
+
+class KubaGame:
+    """"""
+    def __init__(self, (Player_a, color_a), (player_b, color_b)):
+        """"""
+        pass
+
+    def get_current_turn(self):
+        """"""
+        pass
+
+    def make_move(self, player_name, coordinates = (), direction):
+        """"""
+        pass
+
+    def get_winner(self):
+        """"""
+        pass
+
+    def get_captured(self, player_name):
+        """"""
+        pass
+
+    def get_marble(self, coordinates = ()):
+        """"""
+        pass
+
+    def get_marble_count(self):
+        """"""
+        pass
