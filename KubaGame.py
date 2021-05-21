b4d43a09e65acb751aa7c5ab4d05b6312f580cf8
rotorhead7700 <brogdonk@oregonstate.edu>
2021-05-20 23:30:12 -0400

    game board settled, used pass for other methods so far

--- KubaGame.py
+++ KubaGame.py
@@ -7,21 +7,21 @@ class KubaGame:
     def __init__(self, (Player_a, color_a), (player_b, color_b)):
         """Creates a Kuba game board with two players playing either black or white"""
         self._game_board = [[""] * 7 for x in range(7)]  # initializes a 7x7 game board
-        self._game_board[0][0],  self._game_board[0][1],  self._game_board[1][0],  self._game_board[1][1] = "W"
-        self._game_board[5][5], self._game_board[5][6], self._game_board[6][5], self._game_board[6][6] = "W"
-        self._game_board[5][0], self._game_board[5][1], self._game_board[6][0], self._game_board[6][1] = "B"
-        self._game_board[0][5], self._game_board[0][6], self._game_board[1][5], self._game_board[1][6] = "B"
+        self._game_board[0][0],  self._game_board[0][1],  self._game_board[1][0],  self._game_board[1][1] = "W", "W", "W", "W"
+        self._game_board[5][5], self._game_board[5][6], self._game_board[6][5], self._game_board[6][6] = "W", "W", "W", "W"
+        self._game_board[5][0], self._game_board[5][1], self._game_board[6][0], self._game_board[6][1] = "B", "B", "B", "B"
+        self._game_board[0][5], self._game_board[0][6], self._game_board[1][5], self._game_board[1][6] = "B", "B", "B", "B"
         for y in range(1, 6):
             self._game_board[y][3] = "R"
             self._game_board[3][y] = "R"
         for j in range(2, 5):
             self._game_board[j][2] = "R"
             self._game_board[j][4] = "R"
-        pass
+        self._current_turn = None  # initializes current turn to None as either player can start the game
 
     def get_current_turn(self):
-        """"""
-        pass
+        """Returns the player whose turn it is to make a move, and returns None if no one has moved yet"""
+        return self._current_turn
 
     def make_move(self, player_name, coordinates = (), direction):
         """"""
